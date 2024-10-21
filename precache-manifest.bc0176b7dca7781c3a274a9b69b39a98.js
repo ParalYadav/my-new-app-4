79d19a697119f6a158346ec4ee24bc299e65b218
ParalYadav <paralyadav050@gmail.com>
2024-10-21 22:05:55 +0530

    Updates

---
+++ precache-manifest.bc0176b7dca7781c3a274a9b69b39a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c46f7548d77e1b953166449110d2486",
+    "url": "/my-new-app-4/index.html"
+  },
+  {
+    "revision": "39f2084f24df82166dd0",
+    "url": "/my-new-app-4/static/css/2.ec2d912d.chunk.css"
+  },
+  {
+    "revision": "39f2084f24df82166dd0",
+    "url": "/my-new-app-4/static/js/2.9efb636d.chunk.js"
+  },
+  {
+    "revision": "9273221017b2c6c158b2e7c7606b0975",
+    "url": "/my-new-app-4/static/js/2.9efb636d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9178f0ec176d432a3437",
+    "url": "/my-new-app-4/static/js/main.3bba4552.chunk.js"
+  },
+  {
+    "revision": "4bd727bbaf7af4227e11",
+    "url": "/my-new-app-4/static/js/runtime-main.b31a115d.js"
+  }
+]);
